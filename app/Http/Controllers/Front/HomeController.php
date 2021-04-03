726017b9e59041918fa5504ee1a11988b90b684c
HardIk Patel <prns.hardik@gmail.com>
2021-04-03 22:37:11 +0530

    20210403-2237

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -15,98 +15,11 @@
 
     class HomeController extends Controller{
         public function index(Request $request){
-            $category_path = _path('category');
-            $categories = Category::select('id', 'name', 'description',
-                                            DB::Raw("CASE
-                                                WHEN ".'image'." != ''
-                                                THEN CONCAT("."'".$category_path."'".", ".'image'.")
-                                                ELSE CONCAT("."'".$category_path."'".", 'default.png')
-                                            END as image")
-                                        )
-                                        ->where(['status' => 'active'])
-                                        ->get();
-
-            $menu = Category::select('id', 'name', 'description',
-                                            DB::Raw("CASE
-                                                WHEN ".'image'." != ''
-                                                THEN CONCAT("."'".$category_path."'".", ".'image'.")
-                                                ELSE CONCAT("."'".$category_path."'".", 'default.png')
-                                            END as image")
-                                        )
-                                        ->where(['status' => 'active'])
-                                        ->inRandomOrder()
-                                        ->limit(5)
-                                        ->get();
-
-            if($menu->isNotEmpty()){
-                $product_path = _path('product');
-                foreach($menu as $row){
-                    $products = Product::select('id', 'name', 'description', 'price',
-                                            DB::Raw("CASE
-                                                WHEN ".'image'." != ''
-                                                THEN CONCAT("."'".$product_path."'".", ".'image'.")
-                                                ELSE CONCAT("."'".$product_path."'".", 'default.png')
-                                            END as image")
-                                        )
-                                        ->where(['category_id' => $row->id, 'status' => 'active'])
-                                        ->get()
-                                        ->toArray();
-
-                    if(!empty($products))
-                        $row->products = $products;
-                    else
-                        $row->products = [];
-                }
-            }
-
-            $review_path = _path('reviews');
-            $reviews = Review::select('id', 'name', 'title', 'message',
-                                    DB::Raw("CASE
-                                        WHEN ".'image'." != ''
-                                        THEN CONCAT("."'".$review_path."'".", ".'image'.")
-                                        ELSE CONCAT("."'".$review_path."'".", 'default.png')
-                                    END as image")
-                                )
-                                ->where(['status' => 'active'])
-                                ->get();
-
-            return view('front.index', ['menu' => $menu, 'categories' => $categories, 'reviews' => $reviews]);
+            return view('front.index');
         }
 
         public function menu(Request $request){
-            $category_path = _path('category');
-            $data = Category::select('id', 'name', 'description',
-                                            DB::Raw("CASE
-                                                WHEN ".'image'." != ''
-                                                THEN CONCAT("."'".$category_path."'".", ".'image'.")
-                                                ELSE CONCAT("."'".$category_path."'".", 'default.png')
-                                            END as image")
-                                        )
-                                        ->where(['status' => 'active'])
-                                        ->get();
-
-            if($data->isNotEmpty()){
-                $product_path = _path('product');
-                foreach($data as $row){
-                    $products = Product::select('id', 'name', 'description', 'price',
-                                            DB::Raw("CASE
-                                                WHEN ".'image'." != ''
-                                                THEN CONCAT("."'".$product_path."'".", ".'image'.")
-                                                ELSE CONCAT("."'".$product_path."'".", 'default.png')
-                                            END as image")
-                                        )
-                                        ->where(['category_id' => $row->id, 'status' => 'active'])
-                                        ->get()
-                                        ->toArray();
-
-                    if(!empty($products))
-                        $row->products = $products;
-                    else
-                        $row->products = [];
-                }
-            }
-
-            return view('front.menu', ['data' => $data]);
+            return view('front.menu');
         }
 
         public function gallery(Request $request){
@@ -115,29 +28,6 @@
 
         public function about(Request $request){
             return view('front.about');
-        }
-
-        public function contact(Request $request){
-            return view('front.contact');
-        }
-
-        public function contact_store(ContactRequest $request){
-            if(!$request->ajax()){ exit('No direct script access allowed'); }
-
-            $crud = [
-                'name' => $request->name,
-                'email' => $request->email,
-                'phone' => $request->phone,
-                'subject' => $request->subject,
-                'message' => $request->message
-            ];
-
-            $contact = Contact::create($crud);
-
-            if($contact)
-                return response()->json(['code' => 200 ,'message' => 'Thanks For Contact us, we will take actions sortly.']);
-            else
-                return response()->json(['code' => 201 ,'message' => 'Something went wrong, please try again later.']);
         }
 
         public function testimonial(Request $request){
@@ -161,20 +51,7 @@
         }
 
         public function shop(Request $request){
-            $categories = Category::select('id', 'name', 'description')
-                                    ->where(['status' => 'active'])
-                                    ->get();
-
-            $selected = '3';
-            return view('front.shop', ['categories' => $categories, 'selected' => 1]);
-        }
-
-        public function shop_ajax(Request $request){
-            $categories = Category::select('id', 'name', 'description')
-                                    ->where(['status' => 'active'])
-                                    ->get();
-
-            return view('front.shop', ['categories' =>$categories]);
+            return view('front.shop');
         }
 
         public function product_detail(Request $request){
@@ -208,5 +85,28 @@
             }else{
                 return response()->Json(['code' => 201, 'message' => "Somthing Went Wrong !"]);
             }
+        }
+
+        public function contact(Request $request){
+            return view('front.contact');
+        }
+
+        public function contact_store(ContactRequest $request){
+            if(!$request->ajax()){ exit('No direct script access allowed'); }
+
+            $crud = [
+                'name' => $request->name,
+                'email' => $request->email,
+                'phone' => $request->phone,
+                'subject' => $request->subject,
+                'message' => $request->message
+            ];
+
+            $contact = Contact::create($crud);
+
+            if($contact)
+                return response()->json(['code' => 200 ,'message' => 'Thanks For Contact us, we will take actions sortly.']);
+            else
+                return response()->json(['code' => 201 ,'message' => 'Something went wrong, please try again later.']);
         }
     }
